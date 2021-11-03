70490ba732bf49d08ad4507538e66d984da63928
TheOneTrueNoodle <72693746+TheOneTrueNoodle@users.noreply.github.com>
2021-11-03 20:32:12 +0000

    Developed a day transition system

---
+++ Emotion_Game/Assets/Scripts/NextDayScript.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class NextDayScript : MonoBehaviour
+{
+    public Animator transition;
+    public float TransitionTime = 1f;
+    public string NextDay;
+
+    public void StartTransition()
+    {
+        StartCoroutine(LoadDay());
+    }
+
+    IEnumerator LoadDay()
+    {
+        Scene currentScene = SceneManager.GetActiveScene();
+        
+        transition.SetTrigger("Start");
+        AsyncOperation asyncLoad = SceneManager.LoadSceneAsync(NextDay, LoadSceneMode.Additive);
+        
+        while (!asyncLoad.isDone)
+        {
+            yield return null;
+        }
+        
+        SceneManager.UnloadSceneAsync(currentScene);
+    }
+}
