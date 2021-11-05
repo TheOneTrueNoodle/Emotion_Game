1c86d84e0012fa869930ed5c357f51290ab371b2
TheOneTrueNoodle <72693746+TheOneTrueNoodle@users.noreply.github.com>
2021-11-05 23:29:01 +0000

    Fixed music

---
+++ Emotion_Game/Assets/Scripts/PlaySong.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlaySong : MonoBehaviour
+{
+    public string song;
+    private static bool playing;
+
+    void Start()
+    {
+        if (playing == false)
+        {
+            FindObjectOfType<AudioManager>().Play(song);
+            playing = true;
+        }
+    }
+}
